9af96faf978fb5df845f6f207bb8f6e1e34975a5
melanie mwendwa <mwendwathuranira@gmail.com>
2025-07-16 17:47:36 +0300

    update

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+// destructure the onAddItem prop
+function ItemForm({ onItemFormSubmit }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+
+   // Add function to handle submissions
+  function handleSubmit(e) {
+    e.preventDefault();
+    const itemData = {
+    name: name,
+    category: category,
+    isInCart: false,
+  };
+   fetch("http://localhost:4000/items", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(itemData),
+  })
+    .then((r) => r.json())
+    .then((newItem) => onItemFormSubmit(newItem));
+}
+
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
